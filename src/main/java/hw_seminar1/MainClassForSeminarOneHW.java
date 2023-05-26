4fdfe9d4fe7d65240d332eefb5762b48ce7e9090
Roman Annenkov <roman.annenkov@gmail.com>
2023-05-26 13:32:17 +0300

    added logics in main

--- src/main/java/hw_seminar1/MainClassForSeminarOneHW.java
+++ src/main/java/hw_seminar1/MainClassForSeminarOneHW.java
@@ -2,6 +2,19 @@ package hw_seminar1;
 
 public class MainClassForSeminarOneHW {
     public static void main(String[] args) {
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.addDrink(new HotDrink("Горячий шоколад", 20, 0.4, 80))
+                .addDrink(new HotDrink("Американо", 10, 0.2, 85))
+                .addDrink(new HotDrink("Капучино", 10, 0.25, 85));
+
+        System.out.println("---status---");
+        System.out.println(coffeeMachine);
+
+        System.out.println("---sale---");
+        System.out.println(coffeeMachine.saleDrink("Капучино"));
+
+        System.out.println("---status---");
+        System.out.println(coffeeMachine);
 
     }
 }
