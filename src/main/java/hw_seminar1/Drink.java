faa9c13cf148aee46c92f2d7e1b2137010c95b3e
Roman Annenkov <roman.annenkov@gmail.com>
2023-05-26 13:05:38 +0300

    added class Drink

---
+++ src/main/java/hw_seminar1/Drink.java
@@ -0,0 +1,31 @@
+package hw_seminar1;
+
+public class Drink {
+    private final String name;
+    private final double price;
+    private final double volumeInLiters;
+
+
+    public Drink(String name, double price, double volumeInLiters) {
+        this.name = name;
+        this.price = price;
+        this.volumeInLiters = volumeInLiters;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getVolumeInLiters() {
+        return volumeInLiters;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Напиток: %s, Цена: %f, Объём: %f", name, price, volumeInLiters);
+    }
+}
